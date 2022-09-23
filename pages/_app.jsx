11947bcd85cc0f5513a5057bf24654dad5bb7d71
João Gabriel <joaogabrielrcardoso@gmail.com>
2022-09-23 17:27:37 -0300

    Adding a General Layout Wrapper Component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
